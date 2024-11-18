558edebad6da74f2c37f05b4a011ee46ab51f6ff
fhpthh <huehongphan0807@gmail.com>
2024-11-18 16:14:36 +0700

    Mô tả thay đổi

--- app/main.py
+++ app/main.py
@@ -1,33 +1,29 @@
-from fastapi import FastAPI
+from fastapi  import FastAPI
 from app.api.auth import auth_router
 from app.api.video_stream import video_router
 from app.services.video_service import start_threads, stop_threads
 
-# Tạo ứng dụng FastAPI
 app = FastAPI(
-    title="Face Emotion Recognition API",
-    description="API for user authentication and video streaming for emotion recognition.",
-    version="1.0.0"
+    title = "Face Emotion Recognition",
+    version = '1.0.0',
 )
 
-app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
-app.include_router(video_router, prefix="/video", tags=["Video Stream"])
+app.include_router(auth_router, prefix='/auth', tags=['Auth'] )
+app.include_router(video_router, prefix='/video', tags=['Video'] )
 
-async def startup_event():
-    start_threads()
-
-async def shutdown_event():
-    stop_threads()
-
-# Tích hợp các hàm khởi động/tắt ứng dụng vào lifecycle của FastAPI
-@app.on_event("startup")
-async def custom_startup():
-    await startup_event()
-
-@app.on_event("shutdown")
-async def custom_shutdown():
-    await shutdown_event()
+@app.on_event('startup')
+async def startup():
+    try:
+        start_threads()
+        print("Bắt đầu ứng dụng")
+    except Exception as e: print(f"Error: {e}")
+@app.on_event('shutdown')
+async def shutdown():
+    try:
+        stop_threads()
+        print("Ngắt kết nối ứng dụng")
+    except Exception as e: print(f"Error: {e}")
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Face Emotion Recognition API"}
+    return {"Welcom to the Face Emotion Recognition App"}
